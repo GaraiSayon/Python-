8c9d552fa6747b2b254c3d4b3811d63c9c8a7dae
GaraiSayon <sayongarai8@gmail.com>
2024-05-20 07:37:52 +0530

    Create python2.py

---
+++ .github/workflows/python2.py
@@ -0,0 +1,14 @@
+
+my_phone = ["mob_name", "screen_size", "battery", "no_of_speaker"]
+mom_phone = {"mob_name", "screen_size", "battery", "no_of_speaker"}
+dad_phone = ("mob_name", "screen_size", "battery", "no_of_speaker")
+
+print(my_phone[1])
+print(mom_phone[0])
+print(dad_phone[2])
+
+#print(type(my_phone))
+#print(type(mom_phone))
+#print(type(dad_phone))
+
+#print(my_phone[1]=5.7)
