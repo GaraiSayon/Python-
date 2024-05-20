e6347caa1b85b06e90008c4eec74970755bfd290
GaraiSayon <sayongarai8@gmail.com>
2024-05-20 07:32:33 +0530

    Create python1.py

---
+++ .github/workflows/python1.py
@@ -0,0 +1,10 @@
+
+mob_name = "iphone5"
+screen_size = 4.7
+is_android = False
+battery = 4000
+no_of_speaker=2
+#print("My mobile is " + mob_name)
+print(mob_name + str(battery))
+#print(mob_name)
+#print(type(mob_name))
